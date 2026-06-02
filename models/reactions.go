0b503da799a1ab446ca322ed9219404dc051431d
Kwesi0023 <humphreykusi@gmail.com>
2026-06-02 12:37:41 +0000

    added the username column

--- models/reactions.go
+++ models/reactions.go
@@ -8,6 +8,7 @@ type Reaction struct {
 	ID        string    `json:"id"`
 	MessageID string    `json:"message_id"`
 	UserID    uint      `json:"user_id"`
+	Username  string    `json:"username,omitempty"`
 	Emoji     string    `json:"emoji"` // The emoji string (e.g., "heart", "fire")
 	CreatedAt time.Time `json:"created_at"`
 }
